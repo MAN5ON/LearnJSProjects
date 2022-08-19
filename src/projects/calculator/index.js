ced92e1524e51f13b44127d62a10b339bb63e250
MAN5ON <dark34rus@mail.ru>
2022-08-19 19:48:33 +0300

    reorganize calc

--- src/projects/calculator/index.js
+++ src/projects/calculator/index.js
@@ -1,30 +1,24 @@
 import React, { useState } from "react";
 import s from "./calc.module.css"
+import { calcState } from "./state";
 
 export const Calculator = () => {
     const [expr, setExpr] = useState('')
-    
+    console.log(calcState.operands);
+
+
     return (
         <div className={s.calculator}>
             <span id='outField' className={s.outField}> {expr} </span>
             <div className={s.buttons}>
-                <button className={s.operand} onClick={() => setExpr(expr + 1)}>1</button>
-                <button className={s.operand} onClick={() => setExpr(expr + 2)}>2</button>
-                <button className={s.operand} onClick={() => setExpr(expr + 3)}>3</button>
-                <button className={s.operator} onClick={() => setExpr(expr + '+')}>+</button>
-                <button className={s.operand} onClick={() => setExpr(expr + 4)}>4</button>
-                <button className={s.operand} onClick={() => setExpr(expr + 5)}>5</button>
-                <button className={s.operand} onClick={() => setExpr(expr + 6)}>6</button>
-                <button className={s.operator} onClick={() => setExpr(expr + '-')}>-</button>
-                <button className={s.operand} onClick={() => setExpr(expr + 7)}>7</button>
-                <button className={s.operand} onClick={() => setExpr(expr + 8)}>8</button>
-                <button className={s.operand} onClick={() => setExpr(expr + 9)}>9</button>
-                <button className={s.operator} onClick={() => setExpr(expr + '*')}>*</button>
-                <button className={s.dot} onClick={() => setExpr(expr + '.')}>.</button>
-                <button className={s.operand} onClick={() => setExpr(expr + 0)}>0</button>
+                <div className={s.operands}>
+                    {Object.values(calcState.operands).map( value => <button id={value} className={s.operand} onClick={() => setExpr(expr + value)}>{value}</button>)}
+                </div>
+                <div className={s.operators}>
+                    {Object.entries(calcState.operators).map(([key, value]) => <button id={key} className={s.operator} onClick={() => setExpr(expr + value)}>{value}</button>)}
+                </div>
                 <button className={s.clear} onClick={() => setExpr('')}>C</button>
-                <button className={s.operator} onClick={() => setExpr(expr + '/')}>/</button>
-                <button className={s.equal}>=</button>
+                <button className={s.equal} onClick={() => setExpr(eval(expr))}>=</button>
             </div>
         </div>
     )
---
+++ src/projects/calculator/state.js
@@ -0,0 +1,12 @@
+export const calcState = {
+    operands: [
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0'
+    ],
+
+    operators: {
+        'plus': '+',
+        'minus': '-',
+        'division': '/',
+        'multi': '*',
+    },
+}
